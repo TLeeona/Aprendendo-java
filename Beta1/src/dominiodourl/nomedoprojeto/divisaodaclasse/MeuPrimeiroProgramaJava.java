a212834beeddbba2e8b90af077b716a28494061a
Leeona Taufner <m.rafaela.f@gmail.com>
2022-05-06 23:27:12 -0300

    Aguardem atualizações
    Primeira semana de estudos Java finalizada.

---
+++ Beta1/src/dominiodourl/nomedoprojeto/divisaodaclasse/MeuPrimeiroProgramaJava.java
@@ -0,0 +1,10 @@
+package dominiodourl.nomedoprojeto.divisaodaclasse;
+// Criar uma variável que recebe 2, criar outra variável que recebe 2 e me retorna a soma.
+public class MeuPrimeiroProgramaJava {
+    public static void main(String[] args) {
+        byte variablefirt = 2;
+        byte variablesecond = 2;
+
+        System.out.println(variablefirt+variablesecond);
+    }
+}
--- Beta1/src/dominiodourl/nomedoprojeto/divisaodaclasse/Meuprimeiroprogramajava.java
+++
@@ -1,7 +0,0 @@
-package dominiodourl.nomedoprojeto.divisaodaclasse;
-
-public class Meuprimeiroprogramajava {
-    public static void main(String[] args) {
-        System.out.println("sou uma menina bonita");
-    }
-}

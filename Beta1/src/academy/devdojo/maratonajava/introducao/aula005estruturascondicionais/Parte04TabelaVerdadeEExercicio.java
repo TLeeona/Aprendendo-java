2af061ee736b769d72abc578f41e5702e94d482d
Maria Rafaela <105024345+TLeeona@users.noreply.github.com>
2022-05-25 17:39:05 -0300

    Tabela Verdade e Exercício
    Aula 5 - parte 4 concluída com sucesso.
    Maratona Java - DevDojo
    
    Aguardem atualizações.

--- Beta1/src/academy/devdojo/maratonajava/introducao/aula005estruturascondicionais/Parte04TabelaVerdadeEExercicio.java
+++ Beta1/src/academy/devdojo/maratonajava/introducao/aula005estruturascondicionais/Parte04TabelaVerdadeEExercicio.java
@@ -1,23 +1,23 @@
-package academy.devdojo.maratonajava.introducao.aula004operadores;
+package academy.devdojo.maratonajava.introducao.aula005estruturascondicionais;
 
-public class Parte03TabelaVerdadeEExercicio {
+public class Parte04TabelaVerdadeEExercicio {
     public static void main(String[] args) {
-/*Tabala Verdade - é o que vai definir o que se tem como resultado de um experessão boaleana quando se
-*tem mais de uma condição.
-*E/and (&&) - todas as expressões dessa tabela precisa ser verdadeira para que o resultado final seja
-*verdadeiro.
-*V && V = V
-*V && F = F
-*F && V = F
-*F && F = F
-*V && V && V && V && V && F = F
-*Ou/or (||) - Desta tabela, o resultado só vai ser falso se todas as expressões forem falsas.
-*V && V = V
-*V && F = V
-*F && V = V
-*F && F = F
-*F && F && F && F && F && V = V
-*/
+        /*Tabala Verdade - é o que vai definir o que se tem como resultado de um experessão boaleana quando se
+         *tem mais de uma condição.
+         *E/and (&&) - todas as expressões dessa tabela precisa ser verdadeira para que o resultado final seja
+         *verdadeiro.
+         *V && V = V
+         *V && F = F
+         *F && V = F
+         *F && F = F
+         *V && V && V && V && V && F = F
+         *Ou/or (||) - Desta tabela, o resultado só vai ser falso se todas as expressões forem falsas.
+         *V && V = V
+         *V && F = V
+         *F && V = V
+         *F && F = F
+         *F && F && F && F && F && V = V
+         */
 
 /*Exercício - Dado um determinado salário, eu quero saber qual o valor que eu tenho que pagar.
 *$0 à 34,712 - 9,70%
@@ -26,14 +26,14 @@ Acima de 68,508 - 49,50%
 */
         float salarioDaPessoaA = 0f;
         String impostoAPagar;
-        
-        if (isSalarioDaPessoaA >= 0 && <= 34.712){
+
+        if (salarioDaPessoaA >= 0 && salarioDaPessoaA <= 34.712) {
             impostoAPagar = "9,70%";
-        } else if (isSalarioDaPessoaA >= 34.713 && <= 68.507){
+        } else if (salarioDaPessoaA >= 34.713 && salarioDaPessoaA <= 68.507) {
             impostoAPagar = "37,55%";
-        } else{
+        } else {
             impostoAPagar = "49,50%";
         }
-        System.out.println (impostoAPagar);
+        System.out.println(impostoAPagar);
     }
 }

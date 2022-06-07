94e743b1b2ae8edac8dec38fb8ad5ccfdc4067ad
Leeona Taufner <m.rafaela.f@gmail.com>
2022-06-07 16:53:03 -0300

    Break
    Aula 06, parte 03 = Concluído com sucesso.
    Maratona Java - Dev Dojo

---
+++ Beta1/src/academy/devdojo/maratonajava/introducao/aula006estruturasderepeticao/Parte03Break.java
@@ -0,0 +1,15 @@
+package academy.devdojo.maratonajava.introducao.aula006estruturasderepeticao;
+
+public class Parte03Break {
+    public static void main(String[] args) {
+        //Parando loop de repetição.
+        //Imprima os primeiros 25 nuúmeros de um dado valor. Por exemplo, 50.
+        int valorMax = 50;
+        for (int i = 0;i<= valorMax;i++){
+            if(i>=25){
+                break;
+            }
+            System.out.println(i);
+        }
+    }
+}  

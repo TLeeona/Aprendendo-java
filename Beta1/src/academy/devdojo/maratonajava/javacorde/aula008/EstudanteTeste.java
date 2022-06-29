2b9806db78a2949916259a5ac64ca7bdca19ea4d
TLeeona <m.rafaela.f@gmail.com>
2022-06-29 15:35:03 -0300

    Introdução à Classes
    Aula 08 - Maratona DevDojo.
    Aguardem atualizações...

---
+++ Beta1/src/academy/devdojo/maratonajava/javacorde/aula008/EstudanteTeste.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacorde.aula008;
+
+public class EstudanteTeste {
+    public static void main(String[] args) {
+        EstudanteDominio estudante = new EstudanteDominio();
+        estudante.nome = "Hinata";
+        estudante.idade = 27;
+        estudante.sexo = 'F';
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+}
